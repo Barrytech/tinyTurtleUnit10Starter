5951c11644d781d74587c60c8cfcf4088e1ea348
Barry <barry@citypolyhs.org>
2017-03-01 21:20:20 +0000

    create the rectangle

--- script.js
+++ script.js
@@ -11,8 +11,22 @@ forward(50);
 right(90);
 forward(50);
 }
+ 
+ stamp();
 
 // Type your function call below
-
-
+function rectangle(){
+forward(80);
+right(90);
+forward(50);
+right(90);
+forward(80);
+right(90);
+forward(50);
+right(90);
+forward(50);
+}
+rectangle();
 stamp();
+
+
